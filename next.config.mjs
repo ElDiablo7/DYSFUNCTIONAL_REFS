fa26674ef4455ea2c216faf5865300b20dc6346e
ElDiablo7 <zacc78@gmail.com>
2026-07-15 18:46:29 +0100

    Add next.config.mjs to disable ESLint and enable standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'standalone',
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
